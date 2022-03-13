4150d7cdcc79f7ed62d699881cdb302bf0bbe3a8
Mia Anne <mia.cameron02@protonmail.com>
2022-03-13 16:10:09 -0700

    Changed type from int to unsigned int to prevent overflow

--- img_to_bin.py
+++ img_to_bin.py
@@ -11,15 +11,19 @@ k = 0
 for imgdir in os.listdir("./" + rootdir):
     k += 1
 
-rgb_arr = np.zeros([k, 256, 256, 3], dtype="int32")
-gray_arr = np.zeros([k, 256, 256], dtype="int32")
+img = Image.open("./" + rootdir + "/" + imgdir)
+data = np.asarray(img.convert('RGB'), dtype="uint8")
+imgDim = data.shape[0]
+
+rgb_arr = np.zeros([k, imgDim, imgDim, 3], dtype="uint8")
+gray_arr = np.zeros([k, imgDim, imgDim], dtype="uint8")
 
 k = 0
 for imgdir in os.listdir("./" + rootdir):
     img = Image.open("./" + rootdir + "/" + imgdir)
-    data = np.asarray(img.convert('RGB'), dtype="int32")
+    data = np.asarray(img.convert('RGB'), dtype="uint8")
     rgb_arr[k] = data
-    gray_arr[k] = np.asarray(img.convert('L'), dtype="int32")
+    gray_arr[k] = np.asarray(img.convert('L'), dtype="uint8")
     k += 1
 
 np.save(rootdir + "_rgb", rgb_arr)
